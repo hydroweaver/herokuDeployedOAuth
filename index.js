6421813d94487a38b302495203d38019d92e8647
Karan Verma <hydroweaver@gmail.com>
2020-09-10 15:36:44 +0530

    ejs fixed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
-app = express();
+const app = express();
+app.use(cors({credentials: true, origin: true}));
+app.use(express.json());
+app.set("view engine","ejs");
 
 app.get('/', (req, res)=>{
     res.render("template", {data: {name: "Test Name", pic: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'}});
