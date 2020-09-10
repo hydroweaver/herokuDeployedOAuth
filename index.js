c474679f1ce3edf02b8c20e16b4b57fee16a00fe
Karan Verma <hydroweaver@gmail.com>
2020-09-10 15:32:52 +0530

    wip

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const express = require('express');
 app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello Heroku!');
     res.render("template", {data: {name: "Test Name", pic: 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png'}});
 }).listen(process.env.PORT || 8888);
