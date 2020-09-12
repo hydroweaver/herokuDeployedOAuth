51fb0cfef59e33e33f46dd482a78ff8c89b22c2a
Karan Verma <hydroweaver@gmail.com>
2020-09-12 23:45:25 +0530

    added app.use(express.static('public'));

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ let authUserChannelCall = {
 
 const app = express();
 app.use(cors({credentials: true, origin: true}));
+app.use(express.static('images'));
 app.use(express.json());
 app.set("view engine","ejs");
 
